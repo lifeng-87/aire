13ab4186961635916cc1269a1be4b02eee52269d
lifeng87 <oscarcoll.930714@gmail.com>
2023-03-19 15:12:19 +0800

    routes

--- src/lib/types/Agument.d.ts
+++ src/lib/types/Agument.d.ts
@@ -1,9 +1,9 @@
 import { ArrayString } from "@skyra/env-utilities";
 
 declare module "@skyra/env-utilities" {
-  export interface Env {
-    OWNERS_ID: ArrayString;
-    DISCORD_TOKEN: string;
-    DEV_GUILDS_ID: ArrayString;
-  }
+	export interface Env {
+		OWNERS_ID: ArrayString;
+		DISCORD_TOKEN: string;
+		DEV_GUILDS_ID: ArrayString;
+	}
 }
---
+++ src/routes/hello-world.ts
@@ -0,0 +1,23 @@
+import {
+	methods,
+	Route,
+	type ApiRequest,
+	type ApiResponse,
+} from "@sapphire/plugin-api";
+
+export class UserRoute extends Route {
+	public constructor(context: Route.Context, options: Route.Options) {
+		super(context, {
+			...options,
+			route: "hello-world",
+		});
+	}
+
+	public [methods.GET](_request: ApiRequest, response: ApiResponse) {
+		response.json({ message: "Hello World" });
+	}
+
+	public [methods.POST](_request: ApiRequest, response: ApiResponse) {
+		response.json({ message: "Hello World" });
+	}
+}
