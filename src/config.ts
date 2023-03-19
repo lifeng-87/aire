e0742847c3fc20d770c27296f1ba951aad0b7a65
lifeng87 <oscarcoll.930714@gmail.com>
2023-03-19 10:28:40 +0800

    @typescript-eslint/require-await

--- src/config.ts
+++ src/config.ts
@@ -1,52 +1,51 @@
 import {
-  ApplicationCommandRegistries,
-  BucketScope,
-  LogLevel,
-  RegisterBehavior,
+	ApplicationCommandRegistries,
+	BucketScope,
+	LogLevel,
+	RegisterBehavior,
 } from "@sapphire/framework";
-import { envParseArray, envParseString } from "@skyra/env-utilities";
+import { envParseArray, envParseString, setup } from "@skyra/env-utilities";
 import { GatewayIntentBits } from "discord.js";
 import { second } from "#utils/common";
 import { join } from "path";
-import { setup } from "@skyra/env-utilities";
 import { mainFolder } from "#utils/constants";
 import type { Config } from "#lib/types/Config";
 
 setup({ path: join(mainFolder, ".env") });
 
 ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(
-  RegisterBehavior.BulkOverwrite
+	RegisterBehavior.BulkOverwrite
 );
 
 const ownersId = envParseArray("OWNERS_ID");
 const discordToken = envParseString("DISCORD_TOKEN");
 const discordBotId = Buffer.from(
-  discordToken.split(".")[0],
-  "base64"
+	discordToken.split(".")[0],
+	"base64"
 ).toString();
 
 export const config: Config = {
-  isDev: process.env.NODE_ENV != "production",
-  discord: {
-    owners: ownersId,
-    token: discordToken,
-    id: discordBotId,
-    devServer: envParseArray("DEV_GUILDS_ID"),
-    options: {
-      disableMentionPrefix: true,
-      intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
-      defaultCooldown: {
-        filteredUsers: ownersId,
-        scope: BucketScope.User,
-        delay: second(10),
-        limit: 2,
-      },
-      logger: {
-        level:
-          envParseString("NODE_ENV") === "production"
-            ? LogLevel.Info
-            : LogLevel.Debug,
-      },
-    },
-  },
+	isDev: process.env.NODE_ENV !== "production",
+	discord: {
+		owners: ownersId,
+		token: discordToken,
+		id: discordBotId,
+		devServer: envParseArray("DEV_GUILDS_ID"),
+		options: {
+			disableMentionPrefix: true,
+			intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
+			defaultCooldown: {
+				filteredUsers: ownersId,
+				scope: BucketScope.User,
+				delay: second(10),
+				limit: 2,
+			},
+			logger: {
+				level:
+					envParseString("NODE_ENV") === "production"
+						? LogLevel.Info
+						: LogLevel.Debug,
+			},
+		},
+	},
 };
--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import "#lib/setup";
 const client = new AireClient();
 
 async function main() {
-  try {
-    client.login();
-  } catch (error) {
-    client.logger.fatal(error);
-    client.destroy();
-    process.exit(1);
-  }
+	try {
+		client.login();
+	} catch (error) {
+		client.logger.fatal(error);
+		client.destroy();
+		process.exit(1);
+	}
 }
 
 void main();
